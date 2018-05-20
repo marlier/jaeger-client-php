eae5872849f4bd44498a35763c7042f90bc9bfa7
Ian Marlier <imarlier@wikimedia.org>
2018-05-20 17:37:51 +0200

    Try reversing the batch creation logic

--- src/Jaeger/Thrift.php
+++ src/Jaeger/Thrift.php
@@ -13,28 +13,16 @@ class Thrift {
 	}
 
 	public static function makeJaegerBatch($spans, Process $process): Batch {
-		$batch = new Batch( [
-			'process' => $process,
-			'spans' => []
-		] );
-
+		$tSpans = [];
 		foreach ( $spans as $span ) {
 			$context = $span->getContext();
-			array_push($batch['spans'],
-				new Span([
-		#			'trace_id' => $context->getTraceId(),
-		#			'name' => $span->getOperationName(),
-		#			'id' => null,
-		#			'parent_id' => $span->getParentId(),
-		#			'annotations' => [],
-		#			'binary_annotations' => [],
-		#			'debug' => false,
-		#			'timestamp' => $span->getStartTime(),
-		#			'duration' => $span->getEndTime() - $span->getStartTime(),
-		#			# 'trace_id_high' => ????
-				])
-			);
+			array_push( $tSpans, new Span( [] ) );
 		}
+		$batch = new Batch( [
+			'process' => $process,
+			'spans' => $tSpans
+		] );
+		
 		return $batch;
 
 	}
